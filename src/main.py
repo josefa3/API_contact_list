0c126746d505e0893dc1548d85f527b9b2e59324
josefa3 <sefaherrera391993@gmail.com>
2021-01-27 23:07:27 -0400

    falta arreglar los metodos

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,71 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
+@app.route('/contact', methods=['GET'])
+def handle_contacts():
+    contacts = Contact.query.all()
+    response_body = []
+    for contact in contacts:
+        response_body.append(contact.serialize()) #serialize pasa un diccionario
     return jsonify(response_body), 200
+
+@app.route('/contact', methods=['POST'])
+def add_new_contact():
+    # First we get the payload json
+    body = request.get_json()
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'full_name' not in body:
+        raise APIException('You need to specify the username', status_code=400)
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
+    if 'address' not in body:
+        raise APIException('You need to specify the address', status_code=400)
+    if 'phone' not in body:
+        raise APIException('You need to specify the phone', status_code=400)
+    # at this point, all data has been validated, we can proceed to inster into the bd
+    new_contact = Contact(full_name=body['full_name'], email=body['email'], address=body['address'], phone=['phone']) #pasamos los parametros
+    db.session.add(new_contact) # añade un contacto en la base de datos, lo deja en cola
+    try:
+       db.session.commit() # intentas que se integre el cambio
+       return jsonify(new_contact.serialize()), 201
+    except Exception as error:
+        print(error.args) 
+        return jsonify("NOT OK"), 500
+
+@app.route('/contact/<contact_id>', methods=['PUT', 'GET'])
+def get_single_contact(id):
+    """
+    Single person
+    """
+    body = request.get_json()
+    if request.method == 'PUT':
+        contact = Contact.query.get(id)
+        if contact is None:
+            raise APIException('Contact not found', status_code=404)
+        if "full_name" in body:
+            contact.full_name = body["full_name"]
+        if "email" in body:
+            contact.email = body["email"]
+        if "address" in body:
+            contact.address = body['address'] 
+        if "phone" in phone:
+            contact.phone = body['phone']
+        db.session.commit()
+        return jsonify(contact.serialize()), 200
+    if request.method == 'GET':
+        contact = Contact.query.get(id)
+        return jsonify(contact.serialize()), 200
+
+    return "Invalid Method", 404
+
+@app.route('/contact/<contact_id>', methods=['DELETE'])
+def delete_contact(id):
+    contact = Contact.query.get(id)
+    if contact is None:
+        raise APIException('Contact not found', status_code=404)
+    db.session.delete(contact)
+    db.session.commit()
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Contact(db.Model):
+    # __tablename__ = 'Contact'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    full_name = db.Column(db.String(40), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(14), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
             "email": self.email,
+            "address": self.address,
+            "phone": self.phone
             # do not serialize the password, its a security breach
         }
